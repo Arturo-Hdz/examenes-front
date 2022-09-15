767d0a05de16618a4590af85eaf6661ebbb1c7a3
baruch hdz <baruchhdz@baruchs-iMac-93.home>
2022-09-15 01:09:45 -0400

    componentes de usuario y admin, estado de usuario que ha iniciado sesion

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
 
 @Component({
@@ -14,7 +15,7 @@ loginData = {
   "password":''
 }
 
-  constructor(private snack:MatSnackBar, private loginService:LoginService) { }
+  constructor(private snack:MatSnackBar, private loginService:LoginService, private router:Router) { }
 
   ngOnInit(): void {
   }
@@ -41,10 +42,28 @@ loginData = {
 
           this.loginService.loginUser(data.token);
           this.loginService.getCurrentUser().subscribe((user:any) => {
+          this.loginService.setUser(user);
             console.log(user);
+
+            if(this.loginService.getUserRole() == "ADMIN"){
+              //dashboard admin
+              // window.location.href= '/admin';
+              this.router.navigate(['admin']);
+              this.loginService.loginStatusSubject.next(true);
+            }else if(this.loginService.getUserRole() == "NORMAL"){
+              //dashboard user
+              // window.location.href= '/user-dashboard';
+              this.router.navigate(['user-dashboard']);
+              this.loginService.loginStatusSubject.next(true);
+            } else{
+              this.loginService.logout();
+            }
           })
       },(error) => {
         console.log(error);
+        this.snack.open('Detalles invalidos, vuelve a intentar!!','Aceptar',{
+          duration:3000
+        })
       }
     )
   }
