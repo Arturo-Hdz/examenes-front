66c6439b736ba6600b994dec9c5bca16728133fd
baruch hdz <baruchhdz@baruchs-iMac-93.home>
2022-09-22 02:04:00 -0400

    listar, guardar categoria y creacion de componente y servicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCategoriaComponent } from './pages/admin/add-categoria/add-categoria.component';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { ViewCategoriasComponent } from './pages/admin/view-categorias/view-categorias.component';
 import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -38,6 +40,14 @@ const routes: Routes = [
     {
       path: '',
       component : WelcomeComponent
+    },
+    {
+      path:'categorias',
+      component:ViewCategoriasComponent
+    },
+    {
+      path:'add-categoria',
+      component:AddCategoriaComponent
     }
       
     ]
---
+++ src/app/pages/admin/add-categoria/add-categoria.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { CategoriaService } from 'src/app/services/categoria.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-add-categoria',
+  templateUrl: './add-categoria.component.html',
+  styleUrls: ['./add-categoria.component.css']
+})
+export class AddCategoriaComponent implements OnInit {
+
+categoria={
+  titulo: '',
+  descripcion:''
+}
+
+  constructor(private categoriaService:CategoriaService, private snack:MatSnackBar, private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  formSubmit(){
+    if(this.categoria.titulo.trim() == '' || this.categoria.titulo == null){
+      this.snack.open("El titulo es requerido!",'',{
+        duration:3000
+      })
+      return ;
+    }
+    this.categoriaService.agregarCategoria(this.categoria).subscribe(
+      (dato:any) => {
+        this.categoria.titulo = '';
+        this.categoria.descripcion = ''
+        Swal.fire('Categoria Agregada', 'La categoria ha sido agregada con exito', 'success');
+        this.router.navigate(['/admin/categorias']);
+      },
+    (error) => {
+      console.log(error);
+      Swal.fire('error!!','Error al guardar la categoria','error')
+    })
+  }
+
+}
